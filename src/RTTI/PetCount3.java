1854c366469ba7e437fe3dd3d932928cf50cc18f
Vasypu <V980327u>
2022-12-29 23:36:02 +0300

    Added new examples

--- src/RTTI/PetCount3.java
+++ src/RTTI/PetCount3.java
@@ -1,17 +1,26 @@
 package RTTI;
 
+/**
+ *  Метод Class.isInstance() предоставляет возможность динамической проверки типа объекта. С помощью этого
+ *  метода в примере PetCount наконец-то можно будет избавиться от громоздких конструкций instanceof.
+ *  <p>
+ *  Для подсчета всех разновидностей Pet карта PetCounter заполняется типами из Literal- PetCreator.allTypes.
+ *  При этом используется класс MapData, который получает Iterable (контейнер allTypes) и константу
+ *  (нуль в данном случае) и заполняет карту ключами из allTypes, ассоциированными со значением 0.
+ */
+
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class PetCount3 {
     static class PetCounter extends LinkedHashMap<Class<? extends Pet>,Integer> {
         public PetCounter() {
-            super(MapData.map(LiteralPetCreator.allTypes, 0));
+            super(MapData.map(LiteralPetCreator.allTypes, 0));      // создает мапу со всеми ссылками на классы животных
         }
         public void count(Pet pet) {
             // Class.isInstance() избавляет от цепочки instanceof:
-            for(Map.Entry<Class<? extends Pet>,Integer> pair : entrySet())
-                if (pair.getKey().isInstance(pet))
+            for(Map.Entry<Class<? extends Pet>,Integer> pair : entrySet())  // проходит по всем ссылкам на классы животных и определяет
+                if (pair.getKey().isInstance(pet))                          // является ли переданный объект животным
                     put(pair.getKey(), pair.getValue() + 1);
         }
         public String toString() {
---
+++ src/RTTI/TypeCounter.java
@@ -0,0 +1,63 @@
+package RTTI;
+
+/**
+ *  Рекурсивный подсчет
+ *  <p>
+ *  Вместо предварительного заполнения также можно воспользоваться методом Class.isAssignableFrom() и
+ *  создать инструмент общего назначения, который не ограничивается подсчетом Pet.
+ *  <p>
+ *  countClass() сначала подсчитывает экземпляры реального типа класса. Затем, если baseType допускает
+ *  присваивание из суперкласса, то countclass() вызывается рекурсивно для суперкласса.
+ *  <p>
+ *  isAssignableFrom() проверяет во время выполнения, что переданный объект действительно принадлежит
+ *  нужной иерархии
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TypeCounter extends HashMap<Class<?>,Integer> {
+    private Class<?> baseType;
+    public TypeCounter(Class<?> baseType) {
+        this.baseType = baseType;
+    }
+    public void count(Object obj) {
+        Class<?> type = obj.getClass();
+        if (!baseType.isAssignableFrom(type))
+            throw new RuntimeException(obj + " неправильный тип: "
+                    + type + ", должен быть подтипом "
+                    + baseType);
+        countClass(type);
+    }
+    private void countClass(Class<?> type) {
+        Integer quantity = get(type);
+        put(type, quantity == null ? 1 : quantity + 1);
+        Class<?> superClass = type.getSuperclass();
+        if (superClass != null && baseType.isAssignableFrom(superClass)) // проверка того что у класса может быть еще один родитель
+            countClass(superClass);                                      // и тогда рекурсивно вызывается метод подсчета
+    }
+    public String toString() {
+        StringBuilder result = new StringBuilder("{");
+        for(Map.Entry<Class<?>, Integer> pair : entrySet()) {
+            result.append(pair.getKey().getSimpleName());
+            result.append("=");
+            result.append(pair.getValue());
+            result.append(", ");
+        }
+        result.delete(result.length()-2, result.length());
+        result.append("}");
+        return result.toString();
+    }
+}
+
+class PetCount4 {
+    public static void main(String[] args) {
+        TypeCounter counter = new TypeCounter(Pet.class);
+        for(Pet pet : Pets.createArray(20)) {
+            System.out.print(pet.getClass().getSimpleName() + " ");
+            counter.count(pet);
+        }
+        System.out.println();
+        System.out.print(counter);
+    }
+}
