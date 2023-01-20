a1f7d99c4a6df6121bcf9ad2f79e0602aa4a62da
Vasypu <V980327u>
2023-01-20 22:55:49 +0300

    Added new RRTI examples

--- src/RTTI/A.java
+++ src/RTTI/A.java
@@ -62,3 +62,56 @@ class HiddenImplementation {
         g.invoke(a);
     }
 }
+
+// Если реализовать интерфейс как закрытый внутренний класс, все равно с помощью рефлексии
+// удается добраться до реализации.
+class InnerA {
+    private static class C implements A {
+        public void f() { System.out.println("public C.f()"); }
+        public void g() { System.out.println("public C.g()"); }
+        void u() { System.out.println("package C.u()"); }
+        protected void v() { System.out.println("protected C.v()"); }
+        private void w() { System.out.println("private C.w()"); }
+    }
+    public static A makeA() { return new C(); }
+}
+
+class InnerImplementation {
+    public static void main(String[] args) throws Exception {
+        A a = InnerA.makeA();
+        a.f();
+        System.out.println(a.getClass().getName());
+        // Reflection still gets into the private class:
+        HiddenImplementation.callHiddenMethod(a, "g");
+        HiddenImplementation.callHiddenMethod(a, "u");
+        HiddenImplementation.callHiddenMethod(a, "v");
+        HiddenImplementation.callHiddenMethod(a, "w");
+    }
+}
+
+// Даже если создать анонимный класс, все равно с помощью рефлексии
+// удается добраться до реализации.
+class AnonymousA {
+    public static A makeA() {
+        return new A() {
+            public void f() { System.out.println("public C.f()"); }
+            public void g() { System.out.println("public C.g()"); }
+            void u() { System.out.println("package C.u()"); }
+            protected void v() { System.out.println("protected C.v()"); }
+            private void w() { System.out.println("private C.w()"); }
+        };
+    }
+}
+
+class AnonymousImplementation {
+    public static void main(String[] args) throws Exception {
+        A a = AnonymousA.makeA();
+        a.f();
+        System.out.println(a.getClass().getName());
+        // Отражение позволяет получить доступ к анонимному классу:
+        HiddenImplementation.callHiddenMethod(a, "g");
+        HiddenImplementation.callHiddenMethod(a, "u");
+        HiddenImplementation.callHiddenMethod(a, "v");
+        HiddenImplementation.callHiddenMethod(a, "w");
+    }
+}
