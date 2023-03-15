770f42f981d60d00aacda2ceb883e176c82a67fa
Vasypu <V980327u>
2023-03-15 22:23:07 +0300

    Added new generic examples

--- src/generics/Apply.java
+++ src/generics/Apply.java
@@ -1,11 +1,27 @@
 package generics;
 
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
+/**
+ *  Компенсация отсутствия латентной типизации часть 2.
+ *  <p>
+ *  Применение метода к последовательности
+ *  <p>
+ *  Рефлексия открывает интересные возможности, но передает всю проверку типов на стадию выполнения,
+ *  что нежелательно во многих ситуациях. Если проверку типов можно обеспечить на стадии компиляции,
+ *  обычно этот вариант более желателен. Но возможно ли совместить проверку типов во время компиляции
+ *  с латентной типизацией?
+ *  Рассмотрим пример, поясняющий суть проблемы. Допустим, требуется создать метод apply(), который
+ *  будет применять произвольный метод к каждому объекту в последовательности. Похоже, в этой ситуации
+ *  интерфейсы не подходят: интерфейсы слишком сильно ограничивают возможности описания «произвольного
+ *  метода»
+ */
+
+// В методе Apply нам повезло, потому что в Java имеется встроенный интерфейс Iterable, который используется
+// библиотекой контейнеров Java. По этой причине метод apply() может принять любой объект, реализующий
+// интерфейс Iterable; к этой категории относятся все классы Collection (такие, как List). Но он также может
+// принять любой другой объект при условии, что он будет реализовывать Iterable, — например, класс SimpleQueue,
 public class Apply {
     public static <T, S extends Iterable<? extends T>> void apply(S seq, Method f, Object... args) {
         try {
@@ -65,4 +81,50 @@ class SimpleQueue<T> implements Iterable<T> {
     public void add(T t) { storage.offer(t); }
     public T get() { return storage.poll(); }
     public Iterator<T> iterator() { return storage.iterator(); }
+}
+
+// Для примера мы обобщим идею Filledlist и создадим параметризованный метод fill(), который получает последовательность
+// и заполняет ее при помощи Generator. Попытка запрограммировать это решение HaJava сталкивается с проблемой: не существует
+// удобного интерфейса Addable (сходного с интерфейсом Iterable из предыдущего примера). Итак, вместо формулировки «любой
+// объект, для которого можно вызвать add()» необходимо использовать формулировку «подтип Collection». Полученный код не
+// является полностью обобщенным, потому что он должен ограничиваться для работы с реализациями Collection. При попытке
+// использования класса, не реализующего Collection, наш обобщенный код работать не будет.
+
+// Не работает с "любым объектом, содержащим add()". Интерфейса
+// "Addable" не существует, так что приходится использовать
+// Collection. В данном случае полноценное обобщение невозможно.
+class Fill {
+    public static <T> void fill(Collection<T> collection, Class<? extends T> classToken, int size) {
+        for(int i = 0; i < size; i++)
+            // Предполагается наличие конструктора по умолчанию:
+            try {
+                collection.add(classToken.newInstance());
+            } catch(Exception e) {
+                throw new RuntimeException(e);
+            }
+    }
+}
+
+class Contract {
+    private static long counter = 0;
+    private final long id = counter++;
+    public String toString() { return getClass().getName() + " " + id; }
+}
+
+class TitleTransfer extends Contract {}
+
+// В приведенном выше случае, поскольку проектировщики Java (по понятным причинам) не видели необходимости в интерфейсе Addable,
+// мы ограничиваемся иерархией Collection, а класс SimpleQueue, несмотря на наличие у него метода add(), не подойдет. Код,
+// ограниченный использованием collection, вряд лй можно считать полностью обобщенным. С латентной типизацией такой проблемы не будет
+class FillTest {
+    public static void main(String[] args) {
+        List<Contract> contracts = new ArrayList<Contract>();
+        Fill.fill(contracts, Contract.class, 3);
+        Fill.fill(contracts, TitleTransfer.class, 2);
+        for (Contract c : contracts)
+            System.out.println(c);
+        SimpleQueue<Contract> contractQueue = new SimpleQueue<Contract>();
+        // Не сработает. Метод fill() недостаточно обобщен:
+        // Fill.fill(contractQueue, Contract.class, 3);
+    }
 }
