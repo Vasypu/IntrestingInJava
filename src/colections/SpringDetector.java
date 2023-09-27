f30f00b2ff26d23150f64c43283ddb8e9e75ff8a
Vasypu <V980327u>
2023-09-28 00:00:06 +0300

    added new examples

--- src/colections/SpringDetector.java
+++ src/colections/SpringDetector.java
@@ -39,13 +39,13 @@ public class SpringDetector {
     // Используем Groundhog или класс, производный от него:
     public static <T extends Groundhog> void detectSpring(Class<T> type) throws Exception {
         Constructor<T> ghog = type.getConstructor(int.class);
-        Map<Groundhog,Prediction> map = new HashMap<Groundhog,Prediction>();
+        Map<Groundhog, Prediction> map = new HashMap<Groundhog,Prediction>();
         for(int i = 0; i < 10; i++)
             map.put(ghog.newInstance(i), new Prediction());
         System.out.println("map = " + map);
         Groundhog gh = ghog.newInstance(3);     // данный ключ не найден в map
         System.out.println("Looking up prediction for " + gh);
-        if(map.containsKey(gh))
+        if (map.containsKey(gh))
             System.out.println(map.get(gh));
         else
             System.out.println("Key not found: " + gh);
