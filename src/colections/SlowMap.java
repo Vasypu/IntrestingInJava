79c4e763249e62381a0bbd35a6232cd3fd3c35bf
Vasypu <V980327u>
2023-10-14 19:04:23 +0300

    Added new examples

--- src/colections/SlowMap.java
+++ src/colections/SlowMap.java
@@ -1,6 +1,7 @@
 package colections;
 
 import containers.Countries;
+import containers.MapEntry;
 
 import java.util.*;
 
@@ -47,35 +48,4 @@ public class SlowMap<K,V> extends AbstractMap<K,V> {
         System.out.println(m.get("BULGARIA"));
         System.out.println(m.entrySet());
     }
-}
-
-class MapEntry<K, V> implements Map.Entry<K, V> {
-    private K key;
-    private V value;
-    public MapEntry(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
-    public K getKey() { return key; }
-    public V getValue() { return value; }
-    public V setValue(V v) {
-        V result = value;
-        value = v;
-        return result;
-    }
-
-    public int hashCode() {
-        return (key==null ? 0 : key.hashCode()) ^
-                (value==null ? 0 : value.hashCode());
-    }
-
-    public boolean equals(Object o) {
-        if(!(o instanceof MapEntry)) return false;
-        MapEntry me = (MapEntry)o;
-        return (key == null ?
-                me.getKey() == null : key.equals(me.getKey())) &&
-                (value == null ?
-                        me.getValue()== null : value.equals(me.getValue()));
-    }
-    public String toString() { return key + "=" + value; }
 }
