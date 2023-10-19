a14985ec7cbaaa62f7756cdec388a8803e10d13e
Vasypu <V980327u>
2023-10-20 01:26:35 +0300

    Added new container examples

---
+++ src/containers/Test.java
@@ -0,0 +1,11 @@
+package containers;
+
+// Среда для проведения хронометражного тестирования контейнеров
+public abstract class Test<C> {
+    String name;
+
+    public Test(String name) { this.name = name; }
+    // Метод переопределяется для разных тестов.
+    // Возвращает фактическое количество повторений теста,
+    abstract int test(C container, TestParam tp);
+}
